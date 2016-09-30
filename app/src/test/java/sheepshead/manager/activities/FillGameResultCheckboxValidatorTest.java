273aca6fa68f76079dcf0be26babd72ea8af2af5
Nicolas Brauner <nicolas.brauner@gmx.de>
2016-09-30 16:37:40 +0200

    10

---
+++ app/src/test/java/sheepshead/manager/activities/FillGameResultCheckboxValidatorTest.java
@@ -0,0 +1,10 @@
+package sheepshead.manager.activities;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas on 29.09.2016.
+ */
+public class FillGameResultCheckboxValidatorTest {
+
+}
---
+++ app/src/test/java/sheepshead/manager/activities/FillGameResultTest.java
@@ -0,0 +1,43 @@
+package sheepshead.manager.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas on 29.09.2016.
+ */
+/*public class FillGameResultTest {
+
+    //Hier ein Beispiel für einen Unittest für FillGameResult
+
+    FillGameResult fillGameResult = new FillGameResult();
+
+    @Test
+    public void givenOnClickShouldWork(){
+
+        whenIsExecuted();
+
+        thenNoExceptionShouldBeThrown();
+    }
+
+    @Test(expected = Exception.class)
+    public void givenOnClickShouldThrowException(){
+
+        whenIsExecuted();
+
+        thenExceptionShouldBeThrown();
+    }
+
+    public void whenIsExecuted(){
+
+    }
+
+    public void thenNoExceptionShouldBeThrown(){
+
+    }
+
+    public void thenExceptionShouldBeThrown(){
+
+    }
+}*/
